876e6f746b2f4109b98db23e589c7af0495f9f10
mdlcacosta <90288796+mdlcacosta@users.noreply.github.com>
2021-10-02 07:31:45 -0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -12,21 +12,23 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((mailserver, port))
-    clientSocket.send()
+    ehlocommand = 'EHLO\r\n'
+    clientSocket.send(ehlocommand.encode())
+    #clientSocket.accept()
         # Fill in end
 
     recv = clientSocket.recv(1024).decode()
-    print(recv)
-    if recv[:3] != '220':
-      print('220 reply not received from server.')
+    #print(recv)
+    #if recv[:3] != '220':
+     #print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    print(recv1)
-    if recv1[:3] != '250':
-        print('250 reply not received from server.')
+    #print(recv1)
+    #if recv1[:3] != '250':
+     #   print('250 reply not received from server.')
 
     # Send MAIL FROM command and print server response.
     # Fill in start
