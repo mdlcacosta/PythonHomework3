ea4029befce48a27ea4b988899664aef09f2ad60
mdlcacosta <90288796+mdlcacosta@users.noreply.github.com>
2021-10-02 07:57:27 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -75,7 +75,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     recv7 = clientSocket.recv(1024).decode()
    # print(recv7)
     # Fill in end
-
+    clientSocket.close()
 
 if __name__ == '__main__':
     smtp_client(1025, '127.0.0.1')
